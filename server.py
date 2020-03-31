3e76ecc2f12021e0b33dd079268e786c332c99d1
Chua Choon Hoe <choonhoe@gmail.com>
2020-03-31 18:08:16 +0800

    added sign in and sign out button

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/<name>") # Revisit decorators if you unclear of this syntax
-def index(name):
-    name = name.upper()
-    return render_template('index.html', name=name)
+@app.route("/") 
+def index():
+    signed_in = False
+    return render_template('index.html', signed_in = signed_in)
 
 @app.route("/another")
 def show():
@@ -14,5 +14,5 @@ def show():
 def display(username):
     return f"Hi {username}"
 
-if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
+if __name__ == '__main__': 
    app.run()

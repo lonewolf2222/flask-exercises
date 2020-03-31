b1c913f2a83cfbf5d4b0c878083f5ff0c83f1b09
Chua Choon Hoe <choonhoe@gmail.com>
2020-03-31 17:50:39 +0800

    showed name from url parameter in template

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/") # Revisit decorators if you unclear of this syntax
-def index():
-    return render_template('index.html')
+@app.route("/<name>") # Revisit decorators if you unclear of this syntax
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
 @app.route("/another")
 def show():
